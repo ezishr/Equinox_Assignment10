c688f0eb26c23d30ae37a8418abb91179ff22159
wclaus21 <clausws@mail.uc.edu>
2025-04-08 13:37:08 -0400

    documentation complete

--- Equinox_Assignment10/ExtractClaus/claus.py
+++
@@ -1,9 +0,0 @@
-from dataloadingPackage.data_loading import *
-
-data = data_loading_function()
-
-def get_url_organization(index):
-    print("url"+data[index]["remote_url"])
-    print("organization"+data[index]["organization"])
-
-
---
+++ Equinox_Assignment10/ExtractClaus/remoteurl.py
@@ -0,0 +1,26 @@
+# File Name : remoteurl.py
+# Student Name: Will Claus
+# email:  clausws@mail.uc.edu
+# Assignment Number: Assignment 10
+# Due Date:   April 10, 2025
+# Course #/Section:   IS 4010 Section 1
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  We are to work with APIs and Json
+
+# Brief Description of what this module does: This module should build a url with a data request and submit it to the API server.
+# Citations: I mainly just used the work we did in class to make this and my group mate's suggestion
+
+# Anything else that's relevant:
+
+
+
+
+from dataloadingPackage.data_loading import *
+
+data = data_loading_function()
+
+def get_url_organization(index):
+    print("url"+data[index]["remote_url"])
+    print("organization"+data[index]["organization"])
+
+
